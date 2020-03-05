e15db6ec3f7fafcb719095b0f6dd04c6e54a5c88
JP Ventura <contact@jpventura.com>
2020-03-04 22:59:46 -0300

    refactor: replace dagger by kodein

--- app/src/main/java/com/jpventura/github/core/dagger/ApplicationScope.kt
+++
@@ -1,7 +0,0 @@
-package com.jpventura.github.core.dagger
-
-import javax.inject.Scope
-
-@Scope
-@Retention
-annotation class ApplicationScope
--- app/src/main/java/com/jpventura/github/core/dagger/FragmentScope.kt
+++
@@ -1,7 +0,0 @@
-package com.jpventura.github.core.dagger
-
-import javax.inject.Scope
-
-@Scope
-@Retention(AnnotationRetention.RUNTIME)
-annotation class FragmentScope
