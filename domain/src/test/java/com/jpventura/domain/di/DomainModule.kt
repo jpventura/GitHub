70d194c710438209cbd46024db645d55f374bae6
JP Ventura <jpventura@users.noreply.github.com>
2021-07-29 09:18:15 -0300

    test(domain): mock use cases injector

--- domain/src/test/java/com/jpventura/domain/di/DomainModule.kt
+++ domain/src/test/java/com/jpventura/domain/di/DomainModule.kt
@@ -2,14 +2,19 @@ package com.jpventura.domain.di
 
 import com.jpventura.core.domain.model.MemoryModel
 import com.jpventura.core.domain.model.MemoryNestedModel
+import com.jpventura.domain.UseCase
 import com.jpventura.domain.bean.Issue
 import com.jpventura.domain.bean.User
+import com.jpventura.domain.interactor.FindRepositoryIssues
+import com.jpventura.domain.interactor.FindRepositoryPullRequests
+import com.jpventura.domain.interactor.FindTrendingRepositories
 import com.jpventura.domain.VersionControlModel
 import com.jpventura.domain.bean.PullRequest
 import com.jpventura.domain.model.RepositoryModel
 import io.reactivex.schedulers.TestScheduler
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
 import org.kodein.di.generic.singleton
 
 fun domainModules() = Kodein.Module("DomainModule") {
@@ -27,6 +32,18 @@ fun domainModules() = Kodein.Module("DomainModule") {
 
     bind<VersionControlModel.UserModel>() with singleton {
         object : MemoryModel<String, User>(), VersionControlModel.UserModel {}
+    }
+
+    bind<UseCase.FindRepositoryIssues>() with singleton {
+        FindRepositoryIssues(instance(), instance())
+    }
+
+    bind<UseCase.FindRepositoryPullRequests>() with singleton {
+        FindRepositoryPullRequests(instance())
+    }
+
+    bind<UseCase.FindTrendingRepositories>() with singleton {
+        FindTrendingRepositories(instance())
     }
 
     bind<TestScheduler>() with singleton {
