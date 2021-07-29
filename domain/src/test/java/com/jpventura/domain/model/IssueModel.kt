dd372e3d7114729fa52e4691af9b64cc1022c35a
JP Ventura <jpventura@users.noreply.github.com>
2021-07-29 09:10:29 -0300

    test(domain): mock issues model

---
+++ domain/src/test/java/com/jpventura/domain/model/IssueModel.kt
@@ -0,0 +1,7 @@
+package com.jpventura.domain.model
+
+import com.jpventura.core.domain.model.MemoryNestedModel
+import com.jpventura.domain.VersionControlModel
+import com.jpventura.domain.bean.Issue
+
+class IssueModel : MemoryNestedModel<String, Int, Issue>(), VersionControlModel.IssueModel
